273c414d2940a7180ea24cdb13336e3c0b7051df
Diego Hernández <diego.hernandez@theksquaregroup.com>
2021-04-23 09:33:27 -0500

    Adjusting DB host name

--- dbConnection/config.php
+++ dbConnection/config.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "amf.admicredit.com";
-$username = "examenexoal2021";
-$password = "examenexoal2021";
+$servername = "localhost";
+$username = "admin";
+$password = "ExamenEXOAL2021";
+$dbname = "amf";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password) or die("Connection failed: " . mysqli_connect_error());
-if(mysqli_select_db($conn, "amf")!=null)
+if(mysqli_select_db($conn, $dbname)!=null)
     echo "<script>console.log('Connected to DB')</script>";
 ?>
